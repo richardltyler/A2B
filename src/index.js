ab6bd8855e0037e6e9313fc144344bf8db36f5fe
Richard Tyler <richardtyler2011@gmail.com>
2021-01-14 18:19:09 -0700

    Style user area in header

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import './css/index.scss';
 import './scripts.js';
-import './fetch.js'
+import './fetch.js';
 
-import './images/icons8-paper-plane-64.png'
+import './images/icons8-paper-plane-64.png';
+import './images/icons8-change-user-50.png';
 
 
 
