b2e8a4c90fcade2273e780aaf6f6355d6bf9a827
Richard Tyler <richardtyler2011@gmail.com>
2021-01-16 11:06:46 -0700

    Fix imports for new js files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './css/index.scss';
-import './scripts.js';
+// import './User.js';
+// import './Destination.js';
+// import './Trip.js';
 import './fetch.js';
+import './scripts.js';
 
 import './images/icons8-paper-plane-64.png';
 import './images/icons8-change-user-50.png';
---
+++ src/scripts.js
@@ -0,0 +1,9 @@
+import './css/index.scss';
+
+import fetchAPI from './fetch';
+
+import User from './User';
+import Destination from './Destination';
+import Trip from './Trip';
+
+
