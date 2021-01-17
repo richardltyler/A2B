fc7e9079f84d7ac37e70a36de5ab088cada447fb
Richard Tyler <richardtyler2011@gmail.com>
2021-01-17 11:28:19 -0700

    Get random user

--- src/scripts.js
+++ src/scripts.js
@@ -6,6 +6,23 @@ import User from './User';
 import Destination from './Destination';
 import Trip from './Trip';
 
-function getRandomIndex() {
-  return Math.floor(Math.random() * dataset.length); 
+let currentUser;
+
+function getRandomIndex(dataset) {
+  return Math.floor(Math.random() * dataset.length);
 }; 
+
+const users = fetchAPI.getData('travelers');
+const trips = fetchAPI.getData('trips');
+
+Promise.all([users, trips])
+// .then(value => console.log(value[1].trips));
+  .then(value => {
+    generateUser(value[0].travelers)
+  })
+
+function generateUser(userData) {
+  const randomIndex = getRandomIndex(userData);
+  currentUser = new User(userData[randomIndex]);
+  console.log(currentUser)
+}
