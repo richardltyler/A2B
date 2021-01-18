ba1392c04d48c30f9144f9f563b24cf9eba76cd1
richardltyler <70095063+richardltyler@users.noreply.github.com>
2021-01-17 18:26:59 -0700

    Revert "Feature/get data"

--- src/Trip.js
+++ src/Trip.js
@@ -9,32 +9,6 @@ class Trip {
     this.status = tripData.status;
     this.suggestedActivities = tripData.suggestedActivities;
   }
-
-  getDestination(destinationData) {
-    this.destination = destinationData.find(destination => destination.id === this.destinationID);
-    
-    return this.destination;
-  }
-
-  getTravelCost() {
-    const lodging = this.destination.estimatedLodgingCostPerDay * this.duration;
-    const flights = this.destination.estimatedFlightCostPerPerson * this.numberOfTravelers;
-    const agentsFee = 1.1;
-
-    return (agentsFee * (lodging + flights)).toFixed(2)
-  }
-
-  getUnixDepartureDate() {
-    return new Date(this.date).getTime();
-  }
-
-  getUnixReturnDate() {
-    const durationTime = this.duration * 86400000;
-    const departureDate = this.getUnixDepartureDate();
-    const returnDate = new Date(departureDate + durationTime).getTime();
-    
-    return returnDate;
-  }
 }
 
 module.exports = Trip;
--- src/User.js
+++ src/User.js
@@ -5,46 +5,9 @@ class User {
     this.travelerType = userData.travelerType;
   }
 
-  getFirstName() {
-    const names = this.name.split(' ');
-    return names[0];
-  }
-
   getTripData(tripData) {
-    const usersTrips = tripData.filter(trip => trip.userID === this.id);
-    this.trips = this.sortTripsByDate(usersTrips);
+    this.trips = tripData;
   }
-
-  sortTripsByDate(trips) {
-    const sortedTrips = trips.sort((a, b) => a.getUnixDepartureDate() - b.getUnixDepartureDate());
-
-    return sortedTrips;
-  }
-
-  getCostOfYearsTravel(currentDate) {
-    const pastYearsTrips = this.getTripsFromPastYear(currentDate);
-    const cost = pastYearsTrips.reduce((acc, trip) => {
-      acc += parseInt(trip.getTravelCost());
-      return acc;
-    }, 0);
-
-    return `$${cost}`;
-  }
-
-   getTripsFromPastYear(currentDate) {
-    const currentYear = currentDate.getFullYear();
-    const pastYearsTrips = this.trips.reduce((acc, trip) => {
-      const tripYear = new Date (trip.getUnixDepartureDate(currentDate)).getFullYear();
-
-      if (tripYear === currentYear - 1) {
-        acc.push(trip);
-      }
-
-      return acc;
-    }, [])
-
-    return pastYearsTrips;
-   }
 }
 
 module.exports = User;
